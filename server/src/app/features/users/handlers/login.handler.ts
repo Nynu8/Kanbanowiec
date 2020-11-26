a0ec3ba73d3405a8acfaa29d0f511308f0d2dc05
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2020-11-26 01:16:43 +0100

    saving token

--- server/src/app/features/users/handlers/login.handler.ts
+++ server/src/app/features/users/handlers/login.handler.ts
@@ -1,17 +1,16 @@
 import { CommandHandler } from "../../../../shared/command-bus";
 import { LOGIN_COMMAND_TYPE, LoginCommand } from "../commands/login.command";
-import { EventDispatcher } from "../../../../shared/event-dispatcher";
-import LoginEvent from "../events/login.event";
 import UserPassword from "../services/user-password.service";
 import { IsNull, Repository } from "typeorm";
 import { UserModel } from "../models/user.model";
 import { TokenModel } from "../models/token.model";
-import AccessToken from "../services/access-token.service";
 import AccessTokenService from "../services/access-token.service";
+import { v4 as uuid } from "uuid";
 
 export interface LoginHandlerDependencies {
   userRepository: Repository<UserModel>;
   accessTokenService: AccessTokenService;
+  tokenRepository: Repository<TokenModel>;
 }
 
 export default class LoginHandler implements CommandHandler<LoginCommand> {
@@ -20,20 +19,25 @@ export default class LoginHandler implements CommandHandler<LoginCommand> {
   constructor(private dependencies: LoginHandlerDependencies) {}
 
   async execute({ payload }: LoginCommand) {
-    const { userRepository } = this.dependencies;
+    const { userRepository, tokenRepository, accessTokenService } = this.dependencies;
     const { username, password } = payload;
     const user = await userRepository.findOne({ username });
 
     if (!user || !UserPassword.compare(password, user.salt, user.password)) {
       throw new Error("Invalid credentials");
     }
+    //v refreshToken v
+    const token = accessTokenService.generateRefreshToken(username);
+    //^ refreshToke ^
+    const accessToken = accessTokenService.generateAccessToken(username);
+    let userId = user.id;
 
-    const refreshToken = this.dependencies.accessTokenService.generateRefreshToken(username);
-    const accessToken = this.dependencies.accessTokenService.generateAccessToken(username);
+    const newToken = TokenModel.create({ id: uuid(), userId, token });
+    await tokenRepository.save(newToken);
 
     return {
       result: true,
-      tokens: { refreshToken, accessToken },
+      tokens: { token, accessToken },
     };
   }
 }
